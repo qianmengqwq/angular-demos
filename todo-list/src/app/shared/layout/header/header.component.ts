364afd08bfb67178843a8221ae3da4f4eaf8a79f
sayoriqwq <2531600563@qq.com>
2024-04-19 17:35:55 +0800

    feat: header

--- todo-list/src/app/shared/layout/header/header.component.ts
+++ todo-list/src/app/shared/layout/header/header.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
-
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [NzPageHeaderModule, NzBreadCrumbModule, NzIconModule],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.css'
+  styleUrl: './header.component.css',
 })
 export class HeaderComponent {
-
+  onBack(): void {
+    console.log('onBack');
+  }
 }
