efb82e18f859d4b35d5a6f575e17a0710fbf8b23
sayoriqwq <2531600563@qq.com>
2024-04-22 17:48:19 +0800

    feat: loginForm基本编写

--- todo-list/src/app/pages/login/login.component.ts
+++ todo-list/src/app/pages/login/login.component.ts
@@ -1,12 +1,57 @@
 import { Component } from '@angular/core';
-
+import {
+  FormControl,
+  FormGroup,
+  NonNullableFormBuilder,
+  Validators,
+} from '@angular/forms';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    NzFormModule,
+    NzInputModule,
+    NzButtonComponent,
+    ReactiveFormsModule,
+    NzIconModule,
+    NzCheckboxModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  constructor(private fb: NonNullableFormBuilder) {}
+  loginForm: FormGroup<{
+    email: FormControl<string>;
+    password: FormControl<string>;
+  }> = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required]],
+  });
 
+  submitForm(): void {
+    if (this.loginForm.valid) {
+      console.log('submit', this.loginForm.value);
+    } else {
+      Object.values(this.loginForm.controls).forEach((control) => {
+        if (control.invalid) {
+          // touched和dirty有什么区别？
+          // control.markAllAsTouched
+          control.markAsDirty();
+          // 加这个和不加有什么用
+          control.updateValueAndValidity({ onlySelf: true });
+        }
+      });
+    }
+  }
+  resetForm(e: MouseEvent) {
+    e.preventDefault();
+    this.loginForm.reset();
+  }
 }
